1326e7540fc611eac5320c72a006a0662e3ee82a
wangziqin <719713496@qq.com>
2021-11-29 22:07:24 +0800

    add_file2

--- fs.js
+++ fs.js
@@ -4,10 +4,18 @@ const dirs = fs.readdirSync('./')
 // console.log('dirs:', dirs)
 
 // 异步读取
-fs.readdir('./s', (err, data) => {
+fs.readdir('./', (err, data) => {
     if (err) {
         console.log('path error:', err)
     } else {
         console.log('dir:', data)
+    }
+})
+// 创建文件夹
+fs.mkdir('./ttt', (err) => {
+    if (err) {
+        console.log('创建失败', err)
+    } else {
+        console.log('创建成功！')
     }
 })
