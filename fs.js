d50190f846d1173de897503c7eb26d065bd76881
wangziqin <719713496@qq.com>
2021-11-29 22:19:42 +0800

    add_file3

--- fs.js
+++ fs.js
@@ -4,18 +4,58 @@ const dirs = fs.readdirSync('./')
 // console.log('dirs:', dirs)
 
 // 异步读取
-fs.readdir('./', (err, data) => {
-    if (err) {
-        console.log('path error:', err)
-    } else {
-        console.log('dir:', data)
-    }
-})
+// fs.readdir('./', (err, data) => {
+//     if (err) {
+//         console.log('path error:', err)
+//     } else {
+//         console.log('dir:', data)
+//     }
+// })
 // 创建文件夹
-fs.mkdir('./ttt', (err) => {
+// fs.mkdir('./ttt', (err) => {
+//     if (err) {
+//         console.log('创建失败', err)
+//     } else {
+//         console.log('创建成功！')
+//     }
+// })
+
+// 覆盖写入 
+// fs.writeFile('test.txt', '今天十个好日子', (err) => {
+//     if (err) {
+//         console.log('error:', err)
+//     } else {
+//         console.log('成功！')
+//     }
+// })
+
+// 追加写入
+// fs.appendFile('test.txt', '今天十个搜索好日子', (err) => {
+//     if (err) {
+//         console.log('error:', err)
+//     } else {
+//         console.log('成功！')
+//     }
+// })
+
+// 读取文件
+// fs.readFile('test.txt', 'utf-8', (err, data) => {
+//     if (err) {
+//         console.log('error:', err)
+//     } else {
+//         console.log('成功读取:', data)
+//     }
+// })
+
+// 判断文件类型
+fs.stat('./fs.js', (err, stats) => {
     if (err) {
-        console.log('创建失败', err)
+        console.log('err!', err)
     } else {
-        console.log('创建成功！')
+        if (stats.isFile()) {
+            console.log('Is File')
+        } else {
+            console.log('Is Dir')
+        }
     }
 })
