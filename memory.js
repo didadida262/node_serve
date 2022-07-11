d5ec58c4420b972a02c0120e6575144ae766c58a
hhvcg <719713496@qq.com>
2022-07-11 18:07:56 +0800

    add:测试内存api

---
+++ memory.js
@@ -0,0 +1,42 @@
+/*
+ * @Author: Hhvcg
+ * @Date: 2022-07-11 15:47:54
+ * @LastEditors: -_-
+ * @Description: 内存篇
+ */
+
+
+const os = require('os')
+
+const format = (name, bytes) => {
+  return name + ': '+ (bytes / 1024 / 1024 ).toFixed(2) + ' MB '
+}
+
+const printMemoryInfo = () => {
+  const memory = process.memoryUsage()
+  let res = 'Process: '
+  for (let key in memory) {
+    res = res + format(key, memory[key])
+  }
+  return res
+}
+
+
+const useMemory = () => {
+  const size = 1 * 1024 * 1024
+  const arr = new Array(size)
+  for (let i = 0 ; i < size; i++) {
+    arr[i] = 0    
+  }
+  return arr
+}
+const total = []
+for (let i = 0; i < 20; i++) {
+  console.log('index---->', i)
+  console.log(printMemoryInfo())
+  console.log('====================================')
+  total.push(useMemory())
+}
+// // 查看系统内存
+// console.log(format('系统总内存', os.totalmem()))
+// console.log(format('系统闲置内存', os.freemem()))
