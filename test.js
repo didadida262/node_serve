c1901c401a46599eb8460ee72ca760f4fc5528cd
hhvcg <719713496@qq.com>
2022-07-19 18:02:21 +0800

    add:查看变量大小神器

--- test.js
+++ test.js
@@ -5,7 +5,11 @@
  * @Description:测试文件
  */
 const path = require('path')
-const node_respPath = __dirname.split(path.sep).slice( 0, __dirname.split(path.sep).length - 1).join('\\') + '\\node_resp'
+const sizeOf = require('object-sizeof')
+// const node_respPath = __dirname.split(path.sep).slice( 0, __dirname.split(path.sep).length - 1).join('\\') + '\\node_resp'
 // console.log('当前项目地址--->', path.join(__dirname))
-console.log('outDirPath', node_respPath)
+// console.log('outDirPath', node_respPath)
 // console.log('basename--->', path.basename('C:\\temp\\myfile.html'))
+const test = [1,10,1]
+
+console.log('诸神退散--->',sizeOf(test))
