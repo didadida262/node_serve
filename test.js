70a7b16953b84f03c8af83f621cfdfd23dbe0608
hhvcg <719713496@qq.com>
2022-07-29 10:12:10 +0800

    ...

--- test.js
+++ test.js
@@ -4,12 +4,6 @@
  * @LastEditors: -_-
  * @Description:测试文件
  */
-const path = require('path')
-const sizeOf = require('object-sizeof')
-// const node_respPath = __dirname.split(path.sep).slice( 0, __dirname.split(path.sep).length - 1).join('\\') + '\\node_resp'
-// console.log('当前项目地址--->', path.join(__dirname))
-// console.log('outDirPath', node_respPath)
-// console.log('basename--->', path.basename('C:\\temp\\myfile.html'))
-const test = [1,10,1]
 
-console.log('诸神退散--->',sizeOf(test))
+let arr = ['1', '2', 'data', 'data']
+console.log(Array.from(new Set(arr)))
